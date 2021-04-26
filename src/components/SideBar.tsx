3e0dd9f6d74a2dae366044717444902b6534531e
Kassio <kassiogluten@gmail.com>
2021-04-26 18:53:41 -0300

    tentativa1

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,32 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from '../components/Button';
+
+interface GenresProps {
+  genres: Array<{
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }>;
+  handleClickButton: (id: number) => void;
+  selectedGenreId: Number;
+}
+
+export function SideBar({ genres, handleClickButton, selectedGenreId }: GenresProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
